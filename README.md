e9ff908cbaa2eec6a962844a1368fd000c9293d5
Jan Seidl <seidl@avast.com>
2016-09-29 14:32:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Recurring::Process - It's new $module
+
+# SYNOPSIS
+
+    use Recurring::Process;
+
+# DESCRIPTION
+
+Recurring::Process is ...
+
+# LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan Seidl <seidl@avast.com>
---
+++ lib/Recurring/Process.pm
@@ -0,0 +1,39 @@
+package Recurring::Process;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Recurring::Process - It's new $module
+
+=head1 SYNOPSIS
+
+    use Recurring::Process;
+
+=head1 DESCRIPTION
+
+Recurring::Process is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Seidl E<lt>seidl@avast.comE<gt>
+
+=cut
+
